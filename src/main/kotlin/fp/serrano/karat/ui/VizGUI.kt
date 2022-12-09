1532c34627cb58a5b49c70667e673e74a015e585
Alejandro Serrano <trupill@gmail.com>
2022-12-09 17:56:30 +0100

    Use temporary file for visualization

--- src/main/kotlin/fp/serrano/karat/ui/VizGUI.kt
+++ src/main/kotlin/fp/serrano/karat/ui/VizGUI.kt
@@ -1,13 +1,19 @@
 package fp.serrano.karat.ui
 
+import kotlin.io.path.createTempFile
 import edu.mit.csail.sdg.alloy4.Computer
 import edu.mit.csail.sdg.alloy4viz.VizGUI
 import edu.mit.csail.sdg.translator.A4Solution
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.pathString
 
+@Suppress("UNCHECKED_CAST")
 fun A4Solution.visualize() {
   var solution = this
   if (solution.satisfiable()) {
-    solution.writeXML("thingy.xml")
+    val tempPath = createTempFile("karat_")
+    // write the first solution
+    solution.writeXML(tempPath.pathString)
     var viz: VizGUI? = null
     val computer = Computer { args ->
       val xmlFilename = (args as Array<String>).first()
@@ -15,7 +21,9 @@ fun A4Solution.visualize() {
       solution.writeXML(xmlFilename)
       viz?.loadXML(xmlFilename, true)
     }
-    viz = VizGUI(true, "thingy.xml", null, computer, null, 2)
+    viz = VizGUI(true, tempPath.pathString, null, computer, null, 2)
+    // at the end remove the file
+    tempPath.deleteIfExists()
   } else {
     println("unsatisfiable")
   }
