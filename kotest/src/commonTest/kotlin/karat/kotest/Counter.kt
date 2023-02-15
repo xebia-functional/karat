40b0b27f8718195b55877052e5f7b58fd681e732
Alejandro Serrano <trupill@gmail.com>
2023-02-15 15:42:22 +0100

    Make clear that tests should fail

--- kotest/src/commonTest/kotlin/karat/kotest/Counter.kt
+++ kotest/src/commonTest/kotlin/karat/kotest/Counter.kt
@@ -1,5 +1,6 @@
 package karat.kotest
 
+import io.kotest.assertions.shouldFail
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
@@ -28,46 +29,54 @@ fun wrong(action: Action, state: Int): Step<Int, Int> =
 
 class StateMachineSpec: StringSpec({
   "at least zero" {
-    checkAgainst(model, -2, ::right) {
-      always(
-        implies(
-          should { it.action shouldBe Action.READ },
-          should { it.response.shouldBeGreaterThanOrEqual(0) }
+    shouldFail {
+      checkAgainst(model, -2, ::right) {
+        always(
+          implies(
+            should { it.action shouldBe Action.READ },
+            should { it.response.shouldBeGreaterThanOrEqual(0) }
+          )
         )
-      )
+      }
     }
   }
   "at least zero, version 2" {
-    checkTraceAgainst(model, -2, ::right) {
-      whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
-      checkCurrent { item<Action, Int, Int> { it.response.shouldBeGreaterThanOrEqual(0) } }
+    shouldFail {
+      checkTraceAgainst(model, -2, ::right) {
+        whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
+        checkCurrent { item<Action, Int, Int> { it.response.shouldBeGreaterThanOrEqual(0) } }
+      }
     }
   }
   "always increasing" {
-    checkAgainst(model, 0, ::wrong) {
-      always(
-        implies(
-          should { it.action shouldBe Action.READ },
-          remember { current ->
-            val rememberedResponse = current.getOrNull()!!.response
-            afterwards(
-              implies(
-                should { it.action shouldBe Action.READ },
-                should { it.response.shouldBeGreaterThanOrEqual(rememberedResponse) }
+    shouldFail {
+      checkAgainst(model, 0, ::wrong) {
+        always(
+          implies(
+            should { it.action shouldBe Action.READ },
+            remember { current ->
+              val rememberedResponse = current.getOrNull()!!.response
+              afterwards(
+                implies(
+                  should { it.action shouldBe Action.READ },
+                  should { it.response.shouldBeGreaterThanOrEqual(rememberedResponse) }
+                )
               )
-            )
-          }
+            }
+          )
         )
-      )
+      }
     }
   }
   "always increasing, version 2" {
-    checkTraceAgainst(model, 0, ::wrong) {
-      whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
-      val previousResponse = remember().getOrNull()!!.response
-      oneOrMoreSteps()
-      whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
-      checkCurrent { item<Action, Int, Int> { it.response.shouldBeGreaterThanOrEqual(previousResponse) } }
+    shouldFail {
+      checkTraceAgainst(model, 0, ::wrong) {
+        whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
+        val previousResponse = remember().getOrNull()!!.response
+        oneOrMoreSteps()
+        whenCurrent { item<Action, Int, Int> { it.action shouldBe Action.READ } }
+        checkCurrent { item<Action, Int, Int> { it.response.shouldBeGreaterThanOrEqual(previousResponse) } }
+      }
     }
   }
 })
