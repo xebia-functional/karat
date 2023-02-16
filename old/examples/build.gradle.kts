dde64870b598d2f65e943066015bf52b23d28bbe
Alejandro Serrano <trupill@gmail.com>
2023-02-16 09:36:00 +0100

    Work on moving examples

---
+++ old/examples/build.gradle.kts
@@ -0,0 +1,20 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  alias(libs.plugins.kotlin.jvm)
+}
+
+dependencies {
+  implementation(files("../vendor/org.alloytools.alloy.dist.jar"))
+  implementation(projects.karatLib)
+}
+
+kotlin {
+  jvmToolchain(8)
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.freeCompilerArgs = listOf("-Xinline-classes", "-Xcontext-receivers")
+}
